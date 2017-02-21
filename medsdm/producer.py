929b85f999c0b8651acaeeed788c15cd2e74bc82
Erin Sheldon <erin.sheldon@gmail.com>
2017-02-21 13:44:21 -0800

    notes

--- medsdm/producer.py
+++ medsdm/producer.py
@@ -10,7 +10,10 @@ TODO:
             - set weight==0 and some bitmask for missing pixels off edge
 
     - stamps too small, resize to "5-sigma", round to 2^N or 3*2^N
+    - stamps should grow symmetrically
     - make sure stamps are the same size in every band
+
+    - try to center the stamp
 
     - set weight map zero for bad bits in bitmask
     - scale the images to common zero point
